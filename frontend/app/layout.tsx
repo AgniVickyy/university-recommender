fc214edf6b5839986a533e4ed097dc3aecc24ac8
J Agnivignesh <130035849+AgniVickyy@users.noreply.github.com>
2026-06-23 11:32:25 +0530

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Recommendation Engineer",
+  title: "Global Ranked Universities",
   description: "Find suitable universities based on your academic profile and preferences",
 };
 
